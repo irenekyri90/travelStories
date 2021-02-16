96e82b337d840162caa9227e2247796e3a9d6c63
Irene Kyritsis <irene.k@live.ca>
2021-02-16 15:06:13 +0100

    add cloudinary to AddStory form

--- src/components/AddStory/AddStory.js
+++ src/components/AddStory/AddStory.js
@@ -27,13 +27,14 @@ class AddStory extends Component {
     const {
       title,
       location,
+      image,
       description
     } = this.state;
 
     const writtenBy = this.props.user._id;
     console.log(writtenBy);
 
-    axios.post('http://localhost:5000/api/stories', {title, location, description, writtenBy}, { withCredentials: true })
+    axios.post('http://localhost:5000/api/stories', {title, location, image, description, writtenBy}, { withCredentials: true })
       .then((response) => {
         console.log(response);
         console.log(response.data);
@@ -44,18 +45,28 @@ class AddStory extends Component {
   
   }
 
-  // axios
-  // .post("http://localhost:5000/auth/upload", uploadData, {
-  //   withCredentials: true,
-  // })
-  // .then((response) => {
-  //   console.log("response is: ", response);
-  //   // after the console.log we can see that response carries 'secure_url' which we can use to update the state
-  //   this.setState({ image: response.data.secure_url });
-  // })
-  // .catch((err) => {
-  //   console.log("Error while uploading the file: ", err);
-  // });
+  handleFileUpload = (e) => {
+    console.log("The file to be uploaded is: ", e.target.files);
+    const file = e.target.files[0];
+
+    const uploadData = new FormData();
+    // image => this name has to be the same as in the model since we pass
+    // req.body to .create() method when creating a new project in '/api/projects' POST route
+    uploadData.append("image", file);
+
+    axios
+      .post("http://localhost:5000/auth/upload", uploadData, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log("response is: ", response);
+        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
+        this.setState({ image: response.data.secure_url });
+      })
+      .catch((err) => {
+        console.log("Error while uploading the file: ", err);
+      });
+  };
 
   render() {
     return (
@@ -66,9 +77,17 @@ class AddStory extends Component {
           <input name="title" type="text" value={this.state.title} onChange={this.handleInput} required/>
 
           <label for="location">Location:</label>
-          <input name="location" type="text" value={this.state.location} onChange={this.handleInput}/>
+          <input name="location" type="text" value={this.state.location} onChange={this.handleInput} required/>
 
-          <p>image</p>
+
+          <label for="image">Image:</label>
+          <input name="image" type="file" onChange={this.handleFileUpload}/>
+          <span>
+            <img src={this.state.image && this.state.image} alt=""/>
+          </span>
+
+
+
 
           <label for="description">Describe your experience</label>
           <input name="description" type="text" value={this.state.description} onChange={this.handleInput}/>
